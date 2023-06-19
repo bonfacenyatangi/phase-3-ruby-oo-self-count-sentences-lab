1f9d9aa6ae739e446413c5e69df34f64ce12f443
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-06-19 23:06:39 +0300

    Tried to understand self

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+    def initialize(string)
+      @string = string
+    end
+  
+    def ends_with_period?
+      @string.end_with?('.')
+    end
+  
+    def ends_with_question_mark?
+      @string.end_with?('?')
+    end
+  
+    def ends_with_exclamation_mark?
+      @string.end_with?('!')
+    end
+  
+    def count_sentences
+      sentences = @string.split('.')
+      sentences.length
+    end
   end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
+  
 end
